ed6f272db47896d7719f05c2e472cf198c14924a
Anas <anas_choukri@hotmail.fr>
2023-02-17 18:00:07 +0100

    views terminé + suppr fichier inutile vue init

---
+++ front/src/views/index.js
@@ -0,0 +1,8 @@
+import home from './home.vue'
+import create from './create.vue'
+import layout from './layout.vue'
+import modify from './modify.vue'
+
+export {
+    home, create, modify, layout
+}
---
+++ front/src/views/layout.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    
+    name: 'layout'
+}
+</script>>
+
+<template>
+    <main> 
+    <router-view></router-view>
+    </main>
+</template>
+
+<style>
+
+</style>
