c5f93c3dd23fb08fd22963cc1ff4e458bd05f720
Anas <anas_choukri@hotmail.fr>
2023-02-17 19:25:00 +0100

    creation page + suppr' fichier inutile vue

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import * as Auth from '../views/authentification'
+import * as Page from '../views/page'
+
+const routes = [
+
+    { path: '/connect', name: 'login', component: Auth.connect },
+    { path: '/inscription', component: Auth.inscription },
+    { path: '/:pathMatch(.*)*', redirect: '/login' },
+
+    {
+        path: '/',
+        name: 'public',
+        redirect: '/connect',
+        component: Public.publicLayout,
+        children: [
+            { path: '/home', component: Page.home },
+            { path: '/create/', component: Page.create, prop: true },
+            { path: '/modify/:id(\\d+)', name: 'modify', component: Page.modify },
+        ],
+    },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(),
+    routes,
 })
 
-export default router
+export default router;
