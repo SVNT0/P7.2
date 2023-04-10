72ee395f3217a6def6e34d6649471bf9b06d0aeb
Anas <anas_choukri@hotmail.fr>
2023-04-10 16:51:48 +0200

    correction soucis multer

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -3,22 +3,22 @@ const multer = require('multer');
 
 // Établissement des extensions d'images concernés
 const MIME_TYPES = {
-  'image/jpg': '.jpg',
-  'image/jpeg': '.jpeg',
-  'image/png': '.png'
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+  'image/png': 'png'
 };
 
 // Définition du lieu de stockage
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, 'images/');
   },
   // Définition du nom du fichier image
   filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    const name = file.originalname.split(extension).join('_');
-    callback(null, name + Date.now() + extension);
+    callback(null, Date.now() + "_" + name);
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({storage: storage}).single('file');
